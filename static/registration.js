deebaadde66506b769de502434baaac886e4615b
Cassandra Beam <cassandrabeam@gmail.com>
2016-11-16 17:16:40 -0500

    style updates

--- static/registration.js
+++ static/registration.js
@@ -3,21 +3,45 @@ var app = angular.module('registration',[]);
 app.controller('registrationCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.user = {
   	'email':'',
-  	'birthday': new Date(),
-  	'gender':'none',
-  	'state':'',
-  	'educationLevel':''
+  	'birthDate': new Date(),
+  	'genderId':'',
+  	'stateId':'',
+  	'educationId':''
   }
   $scope.maxDate = new Date();
   $scope.minDate = new Date(1900, 00, 01);
 
-  $http.get('http://services.groupkt.com/state/get/usa/all', {}).then(function(response){
-  	$scope.stateList = response.data.RestResponse.result;
+  $http.get('http://psuwebmemberservice.azurewebsites.net/api/states', {}).then(function(response){
+  	$scope.stateList = response.data;
   }, function() {
   	$scope.stateList = {};
   });
 
+  $http.get('http://psuwebmemberservice.azurewebsites.net/api/educationLevels', {}).then(function(response){
+    $scope.edList = response.data;
+  }, function() {
+    $scope.edList = {};
+  });
+
+  $http.get('http://psuwebmemberservice.azurewebsites.net/api/genderOptions', {}).then(function(response){
+    $scope.genderList = response.data;
+  }, function() {
+    $scope.genderList = {};
+  });
+
   $scope.submitForm = function(){
-  	//http put 
+    var config = {
+      headers: { 'Content-Type': 'application/json'}
+    },
+      userData = $scope.user,
+      url = 'http://psuwebmemberservice.azurewebsites.net/api/Registration';
+
+      console.log(userData);
+
+  	$http.post(url, userData, config).then( function () {
+      console.log('success');
+    }, function() {
+      console.log('error');
+    });
   }
 }]);
